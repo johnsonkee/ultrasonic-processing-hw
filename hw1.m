ab1dea2116c0c600424594c30fd20dabbb6c69f1
johnsonkee <33377514+johnsonkee@users.noreply.github.com>
2018-06-03 20:53:04 +0800

    hw1

---
+++ hw1.m
@@ -0,0 +1,10 @@
+% Detection principle homework
+a = 1e12;
+tau = 5 * 1e-6;
+f = 1e6;  % f =1Mhz
+fs =  1e9; %sample frequency fs = 1Ghz
+t = linspace(0,2*tau, 2*tau*fs);
+s12 = sin(2*pi*f*t).*exp(-a*(t-tau).^2/2);
+
+plot(t,s12)
+
